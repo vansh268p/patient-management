6a78025d84eb92fdeaf44013c7662a23a44716af
vansh268p <vanshpatel928@gmail.com>
2026-02-20 00:40:11 +0530

    initialize patient service with CRUD operations and validation

---
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientResponseDTO.java
@@ -0,0 +1,51 @@
+package com.pm.patientservice.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+public class PatientResponseDTO {
+    private String id;
+    private String Name;
+    private String email;
+    private String address;
+    private String dateOfBirth;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
